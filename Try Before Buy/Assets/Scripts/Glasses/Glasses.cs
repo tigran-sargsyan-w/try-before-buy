776bf52f1277f9e09bc64d63e0889d9332f4cbf2
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-10-28 22:27:41 +0200

    - feat : default glasses material and prefabs

---
+++ Try Before Buy/Assets/Scripts/Glasses/Glasses.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Glasses
+{
+    public class Glasses : MonoBehaviour
+    {
+        public Material FrameMaterial;
+        public Material LensMaterial;
+        
+        
+    }
+}
