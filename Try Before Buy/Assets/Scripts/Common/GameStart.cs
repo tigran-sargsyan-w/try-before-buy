8d2eb8b03cc853024737db72f0c9eaf7aaef02a6
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2025-01-16 11:51:48 +0100

    -feat : some changes

---
+++ Try Before Buy/Assets/Scripts/Common/GameStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class GameStart : MonoBehaviour
+    {
+        public UiController uiController;
+        private SceneController sceneController;
+      
+
+        private void Start()
+        {
+            InitControllers();
+        }
+
+        private void InitControllers()
+        {
+            sceneController = SceneController.Instance;
+            sceneController.Init();
+            
+            uiController.Init();
+        }
+    }
+}
